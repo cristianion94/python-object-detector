3601e92da19247bb92900c472a7436d6d904bfc9
Cristian Ion <cristian.ion94@gmail.com>
2022-06-10 15:56:40 +0300

    test scan buffer

--- tests/test_scan_buffer.py
+++ tests/test_scan_buffer.py
@@ -5,8 +5,8 @@ from src.object_detector.scan_buffer import SupportedTypes
 
 
 class TestScanBuffer:
-
-    def test_scan_buffer(self, image_buffer) -> None:
+    """Test suite for scanning an image buffer."""
+    def test_scan_buffer(self, image_buffer: bytes) -> None:
         """Tests something"""
         out = scan_buffer(image_buffer)
         result = json.loads(out)
